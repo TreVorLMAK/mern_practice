b8377a7544fe1af0c8b9b1c6cecf8bc57e77db8c
TreVorLMAK <bimalbhandari563@gmail.com>
2024-11-16 21:21:49 +0545

    update

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const express = require('express')
 const connectToDatabase = require('./db/database')
 const app = express()
 
+
+app.use(express.json())
  connectToDatabase()
 
 app.get("/", (req,res)=>{
     res.send("Hello World")
 })
 
+app.post("/blog", (req,res)=>{
+        res.status(200).json({
+            message: "Blog created successfully"
+        })
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running')
