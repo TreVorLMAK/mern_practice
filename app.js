f981e4cba43acd341d7a265005eeeea86caac8c7
TreVorLMAK <bimalbhandari563@gmail.com>
2024-11-18 20:50:25 +0545

    multer configuration

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 require('dotenv').config()
 const express = require('express')
 const connectToDatabase = require('./db/database')
+
 const Blog = require('./model/blogModel')
+const { storage,multer } = require('./middleware/multerConfig')
 const app = express()
-
-
+const upload = multer({storage: storage})
 app.use(express.json())
+
  connectToDatabase()
 
 app.get("/", (req,res)=>{
     res.send("Hello World")
 })
 
-app.post("/blog",async (req,res)=>{
-    const {title,description,image,subtitle} = req.body
+app.post("/blog",upload.single('image'),async (req,res)=>{
+    const {title,description,subtitle} = req.body
+    const image = req.file ? req.file.path : null;
+
+    if (!title || !description || !image || !subtitle) {
+        return res.status(400).json({ message: "Please fill all fields" });
+    }
     await Blog.create({
         title:title,
         description:description,
